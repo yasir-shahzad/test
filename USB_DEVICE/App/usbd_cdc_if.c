037396ae1e166c04a003b59f133a9d1b452a3c27
Yasir Shahzad <Yasirshahzad918@gmail.com>
2025-03-21 12:11:34 +0500

    publish branch

--- USB_DEVICE/App/usbd_cdc_if.c
+++ USB_DEVICE/App/usbd_cdc_if.c
@@ -51,6 +51,7 @@
 /* USER CODE BEGIN PRIVATE_TYPES */
 
 /* USER CODE END PRIVATE_TYPES */
+uint8_t buf[7];
 
 /**
   * @}
@@ -220,11 +221,23 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
   /* 6      | bDataBits  |   1   | Number Data bits (5, 6, 7, 8 or 16).          */
   /*******************************************************************************/
     case CDC_SET_LINE_CODING:
-
+    buf[0] = pbuf[0];
+    buf[1] = pbuf[1];
+    buf[2] = pbuf[2];
+    buf[3] = pbuf[3];
+    buf[4] = pbuf[4];
+    buf[5] = pbuf[5];
+    buf[6] = pbuf[6];
     break;
 
     case CDC_GET_LINE_CODING:
-
+    pbuf[0] = buf[0];
+    pbuf[1] = buf[1];
+    pbuf[2] = buf[2];
+    pbuf[3] = buf[3];
+    pbuf[4] = buf[4];
+    pbuf[5] = buf[5];
+    pbuf[6] = buf[6];
     break;
 
     case CDC_SET_CONTROL_LINE_STATE:
@@ -263,6 +276,8 @@ static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
   /* USER CODE BEGIN 6 */
   USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
   USBD_CDC_ReceivePacket(&hUsbDeviceFS);
+  uint16_t len = *Len;
+  CDC_Transmit_FS(Buf, len);
   return (USBD_OK);
   /* USER CODE END 6 */
 }
--- USB_DEVICE/App/usbd_desc.c
+++ USB_DEVICE/App/usbd_desc.c
@@ -65,8 +65,8 @@
 #define USBD_VID     1155
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     22336
-#define USBD_PRODUCT_STRING_FS     "STM32ComPort"
+#define USBD_PID_FS     0xC180
+#define USBD_PRODUCT_STRING_FS     "DiagnosticDevice"
 #define USBD_CONFIGURATION_STRING_FS     "CDC Config"
 #define USBD_INTERFACE_STRING_FS     "CDC Interface"
 
@@ -164,8 +164,8 @@ __ALIGN_BEGIN uint8_t USBD_FS_DeviceDesc[USB_LEN_DEV_DESC] __ALIGN_END =
   0x00,                       /*bcdUSB */
 #endif /* (USBD_LPM_ENABLED == 1) */
   0x02,
-  0x02,                       /*bDeviceClass*/
-  0x02,                       /*bDeviceSubClass*/
+  0x00,                       /*bDeviceClass*/
+  0x00,                       /*bDeviceSubClass*/
   0x00,                       /*bDeviceProtocol*/
   USB_MAX_EP0_SIZE,           /*bMaxPacketSize*/
   LOBYTE(USBD_VID),           /*idVendor*/
